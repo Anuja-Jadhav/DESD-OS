1802897496345ca00a9bb84d838c9baa8ad562b8
Anuja Jadhav <anujajadhav271@gmail.com>
2020-10-17 21:01:50 +0530

    creating libraries demo

---
+++ arith/arithdemo.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+#include"arith.h"
+
+int main( int argc,const char *argv[])
+{
+ printf("addition is: %d\n",add(10,20));
+ printf("\n difference is: %d\n",sub(10,20));
+ return 0;
+}
