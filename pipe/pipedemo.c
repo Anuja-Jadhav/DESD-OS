420c3f744a330a36a1377fd4dcb20b6a195c70ab
Anuja Jadhav <anujajadhav271@gmail.com>
2020-11-01 10:21:13 +0530

    demo for pipes IPC

---
+++ pipe/pipedemo.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+    unsigned char rbuff[128];
+    pid_t id;
+    int pfd[2];
+    pipe(pfd);
+
+    id = fork();
+
+    if(0== id)
+    { //child process
+        read(pfd[0],rbuff,128);
+        printf("child :%s\n",rbuff);
+        close(pfd[0]);
+    }
+    else
+    { //parent process
+        write(pfd[1],"cdac\n",5);
+        close(pfd[1]);
+    }
+}
