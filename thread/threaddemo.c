3dfc2c0f327e8c720291ca23012b7b57ea803a72
Anuja Jadhav <anujajadhav271@gmail.com>
2020-11-05 15:15:36 +0530

    thread demo

---
+++ thread/threaddemo.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+
+void *thread(void  *arg)
+{
+    printf("thread\n");
+}
+int main()
+{
+    pthread_t tid;
+    printf("brfore thread creation\n");
+    pthread_create(&tid,NULL,thread,NULL);
+    printf("after thread creation\n");
+    sleep(1);
+    printf("after sleep creation\n");
+    return 0;
+    
+}
